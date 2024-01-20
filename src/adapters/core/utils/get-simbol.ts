7d793216faba302f82e780f6af865e7cb559e449
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-21 00:23:02 +0100

    fix when getSymbol return error if key undefined

--- src/adapters/core/utils/get-simbol.ts
+++ src/adapters/core/utils/get-simbol.ts
@@ -3,20 +3,20 @@ import {TradeSequenceType} from "../../types/sequences";
 
 export const getSymbol = (base: string, quot: string, action: "buy" | "sell", allSymbols: any): TradeSequenceType | null => {
 
-    if (allSymbols[base + quot] !== undefined && allSymbols[base + quot].ask !== null) {
+    if (allSymbols[base + quot] !== undefined) {
         // At first check straight pair is exist and have property "ask" not equal null
         return {
             symbol: base + quot,
             action: action,
             price: allSymbols[base + quot][askOrBid(action)]
         }
-    } else if (allSymbols[quot + base] !== undefined && allSymbols[quot + base].ask !== null) {
+    } else if (allSymbols[quot + base] !== undefined) {
         // Then check reversed pair is exist and have property "ask" not equal null
         const reverseAction: "buy" | "sell" = getReverseAction(action)
         return {
             symbol: quot + base,
             action: reverseAction,
-            price: allSymbols[base + quot][askOrBid(reverseAction)]
+            price: allSymbols[quot + base][askOrBid(reverseAction)]
         }
     } else {
         // Return null if don't have satisfy symbol
