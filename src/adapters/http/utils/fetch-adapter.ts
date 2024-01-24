f2e62ee953e9a55c8a1b7ff67b8d0e7a763ddd7f
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-24 23:51:24 +0100

    add _responseMapper to FetchAdapter

--- src/adapters/http/utils/fetch-adapter.ts
+++ src/adapters/http/utils/fetch-adapter.ts
@@ -1,15 +1,32 @@
 import fetch from "node-fetch";
+import {FetchResponseType} from "../../../types/fetch-binance/input";
 
-export class fetchAdapter{
-    static async request (url: string, init: any) {
+export class FetchAdapter {
+    static async request(url: string, init?: any):Promise<FetchResponseType> {
 
         try {
             const response = await fetch(url, init);
-            return await response.json();
-
+            const responseJson:any = await response.json();
+            return  this._responseMapper(responseJson)
         } catch (error) {
-            return {Error: error}
+            throw new Error()
+            console.log(error)
         }
     }
 
+    static _responseMapper(response: any):FetchResponseType {
+        const responseKeys = Object.keys(response);
+        if (responseKeys.length === 2 && responseKeys[0] === "code" && responseKeys[1] === "msg") {
+            return {
+                type: "error",
+                content: response
+            }
+        } else {
+            return {
+                type: "success",
+                content: response
+            }
+        }
+    }
 }
+
