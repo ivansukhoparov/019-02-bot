4b7018538f1e84bd1070cbae858c39715423cec5
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-24 02:32:03 +0100

    move "createSignature" to specify file

---
+++ src/adapters/http/utils/create-signature.ts
@@ -0,0 +1,5 @@
+import crypto from "crypto";
+
+export function createSignature(queryString: string, secret:string, algorithm:string = "sha256") {
+    return crypto.createHmac(algorithm, secret).update(queryString).digest("hex");
+}
