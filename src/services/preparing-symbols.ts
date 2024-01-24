ad3b6a4bc49aeaec5fae7124068fc8b04c0cec93
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-24 23:50:12 +0100

    add comments to preparing-symbols.ts

--- src/services/preparing-symbols.ts
+++ src/services/preparing-symbols.ts
@@ -2,56 +2,84 @@ import {getAllTradableTickers} from "../adapters/http/fetch";
 
 import {generateCombinations} from "./utils/utils";
 import {createTradeSequence} from "./create-trade-sequence";
-import {ActionTimer} from "../common/utils/timer";
 
-
-const getAllCoins=(tradableTickers:any[])=>{
-
-    const result = tradableTickers.reduce((acc:any, item:any) => {
+const getUniqueCoins = (tradableTickers: any[]) => {
+    return tradableTickers.reduce((acc: any, item: any) => {
         if (!acc.includes(item.baseAsset)) {
             return [...acc, item.baseAsset];
         }else if (!acc.includes(item.quoteAsset)){
             return [...acc, item.quoteAsset];
         }
         return acc;
-
     }, []);
-
-    return result;
 }
 
 export const preparingSymbols =async ()=>{
-    //const startTime = +(new Date());
-    const timer =new ActionTimer("preparingSymbols")
-    timer.start()
-    const tradableTickers =  await getAllTradableTickers();
-    const tradableCoins = getAllCoins(tradableTickers)
-     //    .filter((el:any)=> el!=="GAL" ||  el!=="GALA" || el!=="T");
-     // console.log(tradableCoins)
 
+    //------------------------------------------------------------------------------
+
+    //      Get array of symbols(pairs) that trading now
+    //      Get array in view model like this:
+    //      [...
+    //          {symbol: 'THETABTC', baseAsset: 'THETA', quoteAsset: 'BTC', bid: null, ask: null},
+    //          {symbol: 'THETAETH', baseAsset: 'THETA', quoteAsset: 'ETH', bid: null, ask: null},
+    //           ...]
+    const tradableTickers = await getAllTradableTickers();
+
+    //------------------------------------------------------------------------------
+
+    //      Get array of coins that trading now
+    //      collect unique coins names from tradableTickers
+    //      ['ETH',   'LTC',  'BNB',   'NEO', ...]
+    const tradableCoins = getUniqueCoins(tradableTickers)
+        .filter((el: any) => el !== "GAL" || el !== "GALA" || el !== "T");
+    //      Coins "GAL" "GALA" "T" was removed because it make anomalies
+
+    //------------------------------------------------------------------------------
+
+    //      Get array of all possible combinations of elements from tradableCoins
+    //      collect unique coins names from tradableTickers
+    //      Get array in view model like this:
+    //      [...
+    //          [ 'ETH', 'USDT', 'LTO' ],
+    //          [ 'ETH', 'USDT', 'MBL' ],
+    //      ...]
     let allCombinations = generateCombinations(tradableCoins, 3)
        .filter((el: any) => el[1] === "USDT")
-console.log(allCombinations.length)
-    const symbols:any = tradableTickers.reduce((acc:any, el:any)=>{
+    //       combinations with "USDT" in central position was filtered because it is start currency
+
+    //------------------------------------------------------------------------------
+
+    //      Convert tradableTickers array to symbolsDataSet object to increase speed of processing data set
+    //      Now it has view model like this:
+    //      {...
+    //      MAVTUSD: { baseAsset: 'MAV', quoteAsset: 'TUSD', bid: null, ask: null },
+    //      CFXTUSD: { baseAsset: 'CFX', quoteAsset: 'TUSD', bid: null, ask: null },
+    //      ...}
+    const symbolsDataSet: any = tradableTickers.reduce((acc: any, el: any) => {
         acc[el.symbol] = el
         delete acc[el.symbol].symbol
         return  acc
     }, {})
 
-    const allSequences =
-        allCombinations
-            .map((el: any) => createTradeSequence(el, symbols))
-            .filter((el: any) => el !== null)
-            // .reduce((acc: any, el: any,currentIndex) => {
-            //     acc[currentIndex] = el
-            //
-            //     return acc
-            // }, {});
+    //------------------------------------------------------------------------------
 
-    //    console.log(allSequences);
-    //console.log("done in -- " + (+(new Date()) - startTime) / 1000 + "sec")
-    timer.stop();
-    return {symbols, allSequences};
+    //      Get array with sequence trade instuctions and prices for each combination from allCombinations
+    //      Get array in view model like this:
+    //      [...
+    //          {
+    //             firstSymbol: { symbol: 'BNBUSDT', action: 'buy', price: null },
+    //             secondSymbol: { symbol: 'MATICBNB', action: 'buy', price: null },
+    //             thirdSymbol: { symbol: 'MATICUSDT', action: 'sell', price: null }
+    //          },
+    //      ...]
+
+    const sequencesDataSet = allCombinations
+        .map((el: any) => createTradeSequence(el, symbolsDataSet))
+        .filter((el: any) => el !== null)
+
+
+    return {symbolsDataSet, sequencesDataSet};
 
 }
 
