cf1cc31b6bb6bbabcfcf49b91e13bb447320b7f0
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-27 13:15:23 +0100

    add filters to mapper

--- src/types/fetch-binance/mapper.ts
+++ src/types/fetch-binance/mapper.ts
@@ -8,9 +8,11 @@ export const symbolMapper =(input:any):tickerOutputDataType=>{
         bid:null,
         ask: null,
         filters: {
-            minNotional: input.filters.find((filterType: string) => filterType === 'NOTIONAL').minNotional,
-            minQty: input.filters.find((filterType: string) => filterType === 'LOT_SIZE').minQty,
-            minQtyMarket: input.filters.find((filterType: string) => filterType === 'MARKET_LOT_SIZE').minQty
+            minNotional: input.filters.find((filter:any) => filter.filterType === 'NOTIONAL').minNotional,
+            minQty: input.filters.find((filter:any) => filter.filterType === 'LOT_SIZE').minQty,
+            minQtyMarket: input.filters.find((filter:any) => filter.filterType === 'MARKET_LOT_SIZE').minQty,
+            stepSize:input.filters.find((filter:any) => filter.filterType === 'LOT_SIZE').stepSize,
+            stepSizeMarket:input.filters.find((filter:any) => filter.filterType === 'LOT_SIZE').stepSize,
         }
     }
 }
