44e29b362d347ff48a53af989b7d99e4e0733655
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-27 13:15:35 +0100

    _

--- src/services/trade-sequence.ts
+++ src/services/trade-sequence.ts
@@ -2,35 +2,51 @@ import {OrderSide, QuantityType} from "../types/fetch-binance/input";
 import {BinanceAdapter} from "../adapters/http/binance-adapter";
 import {TradeInstructionType, TradeSequenceType} from "../types/sequences";
 
-const seq: TradeSequenceType = {
-    firstSymbol: {symbol: 'BNB/USDT', currentCurrency: 'USDT', action: 'buy', price: null},
-    secondSymbol: {symbol: 'MATIC/BNB', currentCurrency: 'BNB', action: 'buy', price: null},
-    thirdSymbol: {symbol: 'MATIC/USDT', currentCurrency: 'USDT', action: 'sell', price: null}
-}
 
-const tradeInstruction: TradeInstructionType = seq.firstSymbol;
 
 export const tradeThis = async (instruction: TradeInstructionType) => {
-    const symbol = instruction.symbol.replace("/", "")
-    const side: OrderSide = instruction.action
-    const {quantityCurrency, quantityType} = getQuantityType(instruction)
-    const quantityAmount = await BinanceAdapter.getCurrencyBalance(quantityCurrency)
-    return  BinanceAdapter.placeOrder(symbol, quantityType, quantityAmount, side)
+    console.log(instruction.symbol);
+    const symbol = instruction.symbol.replace("/", "");
+    const side: OrderSide = instruction.action;
+    const {quantityCurrency, quantityType} = getQuantityType(instruction);
+    const currencyAmount = await BinanceAdapter.getCurrencyBalance(quantityCurrency);
+    const quantityAmount = roundDownNumber(currencyAmount, +instruction.filters.stepSize);
+    return  BinanceAdapter.placeOrder(symbol, quantityType, quantityAmount, side);
 }
 
 type s= "firstSymbol"|"secondSymbol"|"thirdSymbol"
 
 export const  tradeAllSequence =async (sequence: TradeSequenceType) => {
-    const steps:s[]=  Object.keys(sequence).map((el)=> el as s )
-//["firstSymbol","secondSymbol","thirdSymbol"]
-    for (let i = 0; i < steps.length; i++) {
-        const step: TradeInstructionType = sequence[steps[i]]
-        //console.log(step)
-        const result=  await tradeThis(sequence[steps[i]]);
-console.log(step.symbol)
-        console.log(result.content)
-    }
 
+
+
+        const result1=  await tradeThis(sequence.firstSymbol);
+ console.log("trade sequence " + result1.type)
+    console.log(sequence.firstSymbol)
+    console.log("============================================================")
+    const result2=  await tradeThis(sequence.secondSymbol);
+console.log("trade sequence " + result2.type)
+    console.log(sequence.secondSymbol)
+    console.log("============================================================")
+    const result3=  await tradeThis(sequence.thirdSymbol);
+ console.log("trade sequence " + result3.type)
+    console.log(sequence.thirdSymbol)
+    console.log("============================================================")
+        // console.log("trade sequence " + result.type)
+      // console.log(i);
+      //   console.log(result.content)
+        // console.log(result.content.status)
+
+    //      OLD
+    // const steps:s[]=  Object.keys(sequence).map((el)=> el as s )
+    // for (let i = 0; i < steps.length; i++) {
+    //     const step: TradeInstructionType = sequence[steps[i]]
+    //     const result=  await tradeThis(step);
+    //     // console.log("trade sequence " + result.type)
+    //     console.log(i);
+    //     console.log(result.content)
+    //     // console.log(result.content.status)
+    // }
 }
 
 const getQuantityType = (instruction: TradeInstructionType): {
@@ -51,10 +67,15 @@ const getQuantityType = (instruction: TradeInstructionType): {
     }
 }
 
-
 const getSymbolBase = (symbol: string) => {
     return symbol.split("/")[0]
 }
 const getSymbolQuote = (symbol: string) => {
     return symbol.split("/")[1]
+}
+
+export function roundDownNumber(number1:number, number2:number) {
+    const decimalPlaces = (number2.toString().split('.')[1] || []).length;
+    const multiplier = Math.pow(10, decimalPlaces);
+    return Math.floor(number1 * multiplier) / multiplier;
 }
