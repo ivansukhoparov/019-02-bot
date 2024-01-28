96cceda913bf03fd21d7776ba1cc10472f5bfaf4
ivansukhoparov <rbcdaaa@gmail.com>
2024-01-29 00:53:33 +0100

    add tests for getSymbol BinanceService's method

---
+++ __tests__/unit-tests/binance-service.test.ts
@@ -0,0 +1,58 @@
+import {BinanceService} from "../../src/application/binance-service";
+import {symbolsDataSet_test} from "../tests-data-sets/symbols-data-set";
+import {orderAction, orderQuantity} from "../../src/common/common";
+
+
+describe("binance-services methods tests", () => {
+
+    it("_getSymbol method must return correct data", () => {
+        const binanceService   = new BinanceService(symbolsDataSet_test);
+        const {symbol,action, quantityType}:any =  binanceService._getSymbol('XAI','USDT');
+        expect(symbol).toBe("XAI/USDT");
+        expect(action).toBe(orderAction.sell);
+        expect(quantityType).toBe(orderQuantity.base);
+    })
+
+    it("_getSymbol method must return correct data", () => {
+        const binanceService   = new BinanceService(symbolsDataSet_test);
+        const {symbol,action, quantityType}:any =  binanceService._getSymbol('USDT','XAI');
+        expect(symbol).toBe("XAI/USDT");
+        expect(action).toBe(orderAction.buy);
+        expect(quantityType).toBe(orderQuantity.quote);
+    })
+
+    it("_getSymbol method must return null if symbol not exist", () => {
+        const binanceService   = new BinanceService(symbolsDataSet_test);
+        expect(()=>{
+            binanceService._getSymbol('USDT','MNAI');
+        }).toThrow()
+
+    })
+
+
+
+    // it("function must return correct sequence", () => {
+    //
+    //
+    //
+    // })
+    //
+    //
+    // it("function must return null if symbol not in list", () => {
+    //
+    //
+    // })
+    // it("function must return null if symbol not in list", () => {
+    //
+    //
+    // })
+    //
+    // it("function must return null if symbol not in list", () => {
+    //
+    //
+    //
+    // })
+    //
+
+
+})
