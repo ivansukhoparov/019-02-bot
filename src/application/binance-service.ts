3c06970f28ef91753efcc758b349c5ad131d5794
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-03 03:37:15 +0100

    refactoring: back to static

--- src/application/binance-service.ts
+++ src/application/binance-service.ts
@@ -6,13 +6,6 @@ import {orderAction, orderQuantity} from "../common/common";
 export class BinanceService {
 
 // this method received current currency, amount of it and target currency and create order
-    private adapter: BinanceAdapter;
-    public symbolsDataSet: any;
-
-    constructor(symbolsDataSet: any) {
-        this.adapter = new BinanceAdapter();
-        this.symbolsDataSet = symbolsDataSet;
-    }
 
     // 'PSG/TRY': {
     //     baseAsset: 'PSG',
@@ -27,56 +20,46 @@ export class BinanceService {
     //         stepSizeMarket: '0.10000000'
     //     }
     // }
-    public async createOrder(currentCurrency: string, targetCurrency: string, amount?: number) {
-        const {symbolName, action, quantityType}: any = this._getSymbol(currentCurrency, targetCurrency)
-        const symbol = this.symbolsDataSet[symbolName]
+    static async createOrder(currentCurrency: string, targetCurrency: string, amount: number, symbolsDataSet: any) {
+        const {symbolName, action, quantityType}: any = this._getSymbol(currentCurrency, targetCurrency,symbolsDataSet)
+        const symbol = symbolsDataSet[symbolName]
         const side: OrderSide = action;
 
-        const balance = await BinanceAdapter.getCurrencyBalance(currentCurrency);
-
-        if (!amount || amount < +balance) {
-            amount = +balance;
-        }
-
-        if (amount) {
             if (quantityType === orderQuantity.base) {
                 amount = roundDownNumber(amount, symbol.filters.stepSize);
-                const amountInQuote = amount*+symbol.ask;
+                const amountInQuote = amount * +symbol.price;
                 if (amountInQuote < symbol.filters.minNotional) {
-                    throw new Error("Do not have enough amount, or enter grater amount")
+                    throw new Error("Don't have information about price")
                 }
             } else if (quantityType === orderQuantity.quote) {
                 if (amount < symbol.filters.minNotional) {
-                    throw new Error("Do not have enough amount, or enter grater amount")
+                    throw new Error("enter grater amount")
                 }
             }
-
             return BinanceAdapter.placeOrder(symbolName, quantityType, amount, side);
-        } else {
-            throw new Error("currency does not exist, or can't get amount")
-        }
     }
 
-    public _getSymbol(currentCurrency: string, targetCurrency: string) {
+    static _getSymbol(currentCurrency: string, targetCurrency: string, symbolsDataSet: any) {
 
-        if (this.symbolsDataSet[currentCurrency + "/" + targetCurrency] !== undefined) {
+
+        if (symbolsDataSet[currentCurrency + "/" + targetCurrency] !== undefined) {
             // At first check straight pair is exist and have property "ask" not equal null
             // For example we have ETH and want get USDT
             // ETH/USDT has found, and we must create sell order
             // and currency amount must be in base
-            const symbol = currentCurrency + "/" + targetCurrency;
+            const symbolName = currentCurrency + "/" + targetCurrency;
             const action = orderAction.sell;
             const quantityType = orderQuantity.base
-            return {symbol, action, quantityType}
-        } else if (this.symbolsDataSet[targetCurrency + "/" + currentCurrency] !== undefined) {
+            return {symbolName, action, quantityType}
+        } else if (symbolsDataSet[targetCurrency + "/" + currentCurrency] !== undefined) {
             // Then check reversed pair is exist and have property "ask" not equal null
             // For example we have USDT and want get ETH
             // ETH/USDT has found, and we must create buy order
             // and currency amount must be in quote
-            const symbol = targetCurrency + "/" + currentCurrency;
+            const symbolName = targetCurrency + "/" + currentCurrency;
             const action = orderAction.buy;
             const quantityType = orderQuantity.quote
-            return {symbol, action, quantityType}
+            return {symbolName, action, quantityType}
         } else {
             // Return null if don't have satisfy symbol
             throw new Error("Symbol does not exist");
