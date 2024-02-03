586b89b774126e2d3dde0b13199dde27de3b8dc0
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-03 03:35:49 +0100

    _

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {BinanceAdapter} from "./adapters/http/binance-adapter";
 
 
-import {getAllTradableSymbols} from "./services/get-all-tradable-symbols";
-import {createSequencesDataSet, createSymbolsDataSet} from "./services/preparing-symbols";
+import {addPricesToSymbolsArray, getAllTradableSymbols} from "./services/get-all-tradable-symbols";
+import {createSymbolsDataSet} from "./services/preparing-symbols";
 import {wsUpdate} from "./adapters/websokets/websoket-adapter";
+import {BinanceService} from "./application/binance-service";
+import {copyFileSync} from "fs";
+import {createSequencesDataSet} from "./services/create-sequences-data-set";
+import {logCurrencyAmount} from "./common/utils/logs";
 
 require('dotenv').config();
 
@@ -15,9 +19,19 @@ const app = async () => {
 }
 
 
+
+
 const startApp = async ()=>{
     try {
+        const allTradableSymbols = await getAllTradableSymbols();
+        const symbolsDataSet = await createSymbolsDataSet(allTradableSymbols);
+       await logCurrencyAmount("USDC")
+      await  logCurrencyAmount("ETH")
+const SB = await  BinanceService.createOrder("USDC","ETH",1,symbolsDataSet)
 
+        await logCurrencyAmount("USDC")
+        await  logCurrencyAmount("ETH")
+        console.log(SB)
     } catch (err) {
         console.log(err)
     }
