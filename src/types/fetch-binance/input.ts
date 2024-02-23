5c62dcf802240a69082393362b017d24601e0897
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-23 13:14:08 +0100

    add: add RestApiTickerInfo type for api response

--- src/types/fetch-binance/input.ts
+++ src/types/fetch-binance/input.ts
@@ -150,3 +150,27 @@ export type FetchResponseType = {
 
 
 export type QuantityType = "quantity" | "quoteOrderQty"
+
+export type RestApiTickerInfo=    {
+        symbol: string
+        priceChange: string
+        priceChangePercent: string
+        weightedAvgPrice: string
+        prevClosePrice: string
+        lastPrice: string
+        lastQty: string
+        bidPrice: string
+        bidQty: string
+        askPrice: string
+        askQty: string
+        openPrice: string
+        highPrice: string
+        lowPrice: string
+        volume: string
+        quoteVolume: string
+        openTime: number
+        closeTime: number
+        firstId: number
+        lastId: number
+        count: number
+    }
