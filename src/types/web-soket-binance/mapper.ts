5e274486b96d19d2211b1dc9797e89e455f0b561
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-26 19:53:32 +0100

    add: add mapper that map binance websocket data to right view model

--- src/types/web-soket-binance/mapper.ts
+++ src/types/web-soket-binance/mapper.ts
@@ -1,9 +1,24 @@
-import {tickerDataType, tickerReceivedDataType} from "./input";
+import {tickerDataType, TickerReceivedDataType} from "./input";
+import {MarketUpdateDataType} from "./output";
 
-export const tickerMapper = (ticker:tickerReceivedDataType):tickerDataType =>{
+export const tickerMapper = (ticker:TickerReceivedDataType):tickerDataType =>{
 	return {
 		symbol: ticker.s,
 		bid: ticker.b,
 		ask: ticker.a,
 	};
 };
+
+export const marketDataMapper =  (ReceivedData:TickerReceivedDataType):MarketUpdateDataType   =>{
+	return{
+		symbol: ReceivedData.s,
+		priceChange24Per: +ReceivedData.P,
+		bidPrice: +ReceivedData.b,
+		bidQty: +ReceivedData.B,
+		askPrice: +ReceivedData.a,
+		askQty: +ReceivedData.A,
+		baseTradeQty24: +ReceivedData.v,
+		quoteTradeQty24: +ReceivedData.q,
+		numberOfTransactions: +ReceivedData.n,
+	}
+}
