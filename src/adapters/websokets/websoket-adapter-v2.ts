59de0acc2b59fc503347dd9ec11c7bc8fb319ad5
ivansukhoparov <rbcdaaa@gmail.com>
2024-02-26 19:52:30 +0100

    refactoring: connect TradeCore instance to websocket, add received data mapping function

---
+++ src/adapters/websokets/websoket-adapter-v2.ts
@@ -0,0 +1,52 @@
+import WebSocket from "ws";
+
+import {appMode, appSettings} from "../../settings/settings";
+import {APP_MODES} from "../../common/common";
+import {MarketUpdateDataType} from "../../types/web-soket-binance/output";
+import {marketDataMapper} from "../../types/web-soket-binance/mapper";
+import {TradeCore} from "./core";
+
+
+
+let combinedStreamsUrl: string
+
+
+if (appMode === APP_MODES.test) {
+    combinedStreamsUrl = "wss://testnet.binance.vision/ws/!ticker@arr"
+} else {
+    const streamNames = ["!ticker@arr"];
+    combinedStreamsUrl = `wss://stream.binance.com:9443/stream?streams=${streamNames.join("/")}`;
+}
+
+const connection = new WebSocket(combinedStreamsUrl);
+
+export const wsUpdate2 = (tradeCore:TradeCore) => {
+
+    connection.onopen = async () => {
+        console.log("Connected to Binance combined WebSocket");
+    };
+
+    connection.onmessage = async (e) => {
+        try {
+
+            let marketData = JSON.parse(e.data.toString());
+            if (appMode !== APP_MODES.test) marketData = marketData.data
+            const mappedMarketData: MarketUpdateDataType[] = marketData.map(marketDataMapper)
+            await tradeCore.onDataUpdate(mappedMarketData)
+
+        } catch (error) {
+            console.error("Ошибка при обработке сообщения:", error);
+        }
+    };
+
+
+    connection.onerror = (error) => {
+        console.error("WebSocket error:", error.message);
+    };
+
+    connection.onclose = () => {
+        console.log("Disconnected from Binance WebSocket");
+    };
+};
+
+
