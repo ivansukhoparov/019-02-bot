9a77e03a0612da51f6e25804e5b8fa9b0dc71bd4
ivansukhoparov <rbcdaaa@gmail.com>
2024-03-13 00:48:08 +0100

    _

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {logCurrencyAmount} from "./common/utils/logs";
-import {BinanceHttpAdapter} from "./adapters/http/binance.http.adapter";
+import {BinanceHttpAdapterStatic} from "./adapters/http/binance.http.adapter.static";
 import {appMode, appSettings} from "./settings/settings";
 import {APP_MODES} from "./common/common";
 import {iotaToUsdt} from "./core/utils/test.mode.utils";
@@ -9,13 +9,13 @@ require("dotenv").config();
 
 const startApp = async ()=>{
 	try {
-		console.log("v0.02.4.2")
+		console.log("v0.02.4.3")
 		console.log("APP MODE " + appMode)
 		console.dir(appSettings)
 		console.log("correctedStartAmountResult >0.01 && correctedStartAmount.startAmount>= 6")
 
 		if (appMode === APP_MODES.test) {
-			const balanceTest = await BinanceHttpAdapter.getCurrencyBalance("USDT");
+			const balanceTest = await BinanceHttpAdapterStatic.getCurrencyBalance("USDT");
 			if (balanceTest<100){
 				console.log("low balance")
 				await iotaToUsdt(100)
