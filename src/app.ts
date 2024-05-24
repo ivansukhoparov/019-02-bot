68432136a31b03ad5637f5e7d67413fecac94059
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-24 23:35:34 +0200

    refactoring: remove init() from app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,6 @@
-import {getAllTradableSymbols} from "./services/get-all-tradable-symbols";
-import {createSymbolsDataSet} from "./services/preparing-symbols";
-import {createSequencesDataSet} from "./services/create-sequences-data-set";
-import {TradeCore} from "./core/trade.core";
 import {wsUpdate} from "./adapters/websoket/binance.websoket";
 
-export const init = async () => {
-    const allTradableSymbols = await getAllTradableSymbols();
-    const symbolsDataSet = await createSymbolsDataSet(allTradableSymbols);
-    const sequencesDataSet = await createSequencesDataSet(allTradableSymbols, symbolsDataSet);
-    return new TradeCore(0.1, 100, symbolsDataSet, sequencesDataSet)
-}
+
 export const app = async () => {
     const binanceCore = await init();
     console.log("app initiated")
