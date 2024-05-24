8456fd1f56ab04c5522f8c297f60e397de5bf7ce
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-24 23:34:13 +0200

    add: create AvailableSymbols class

---
+++ src/services/classes/available.symbols.ts
@@ -0,0 +1,49 @@
+import {MarketHttpAdapterInterface} from "../../adapters/http/interfaces/market.http.adapter.interface";
+import {ioc} from "../../composition.root";
+import {TickerOutputDataType} from "../../types/web-soket-binance/input";
+import {symbolMapper} from "../../types/fetch-binance/mapper";
+
+export class AvailableSymbols {
+    private availableSymbols: Array<any> = []
+
+    constructor(protected marketAdapter: MarketHttpAdapterInterface = ioc.marketHttpAdapter) {
+        this.init();
+    }
+
+    init() {
+        // Get all symbols and all prices
+        const allSymbolsPromise = this.marketAdapter.getAllSymbols();
+        const allPricesPromise = this.marketAdapter.getTickerPrices();
+        // const [allSymbols, allPrices] = await
+
+        Promise.all([allSymbolsPromise, allPricesPromise])
+            .then(([allSymbols, allPrices]) => {
+
+                const symbols: TickerOutputDataType[] = allSymbols.content.symbols
+                    .filter((el: any) => el.status === "TRADING")
+                    .map(symbolMapper);
+                //
+                // status: Этот ключ указывает на текущее состояние торговой пары. Если значение status равно "TRADING",
+                // это означает, что пара активна и торгуется. "BREAK" означает, что торговля по этой паре в данный момент
+                // приостановлена или не проводится.
+                //
+
+                const prices = allPrices.content.reduce((acc: any, el: any) => {
+                    acc[el.symbol] = el.price;
+                    return acc;
+                }, {});
+
+                this.availableSymbols = symbols.map((el: any) => {
+                    const symbol = el.baseAsset + el.quoteAsset;
+                    return {
+                        ...el,
+                        price: prices[symbol],
+                    };
+                });
+            })
+    }
+
+    get() {
+        return this.availableSymbols
+    }
+}
