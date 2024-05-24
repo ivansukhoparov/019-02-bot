6d29a0e2199bae76bf7062201a98525c275d7c27
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-24 23:34:36 +0200

    add: create composition.root

---
+++ src/composition.root.ts
@@ -0,0 +1,23 @@
+import {BinanceHttpAdapter} from "./adapters/http/binance.http.adapter";
+import {SymbolsDataSet} from "./services/preparing-symbols";
+import {HttpAdapterInterface} from "./adapters/http/interfaces/http.adapter.interface";
+import {FetchAdapter} from "./adapters/http/utils/fetch.adapter";
+import {MarketHttpAdapterInterface} from "./adapters/http/interfaces/market.http.adapter.interface";
+import {AvailableSymbols} from "./services/classes/available.symbols";
+import {SequencesDataSet} from "./services/classes/sequences.data.set";
+
+
+const httpAdapter: HttpAdapterInterface = new FetchAdapter()
+const marketHttpAdapter: MarketHttpAdapterInterface = new BinanceHttpAdapter()
+
+const availableSymbols: AvailableSymbols = new AvailableSymbols()
+const symbolsDataSet: SymbolsDataSet = new SymbolsDataSet()
+const sequencesDataSet: SequencesDataSet = new SequencesDataSet()
+
+export const ioc = {
+    httpAdapter: httpAdapter,
+    marketHttpAdapter: marketHttpAdapter,
+    availableSymbols: availableSymbols,
+    symbolsDataSet: symbolsDataSet,
+    sequencesDataSet: sequencesDataSet
+}
