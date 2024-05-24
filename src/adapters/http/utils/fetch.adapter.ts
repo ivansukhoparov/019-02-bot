f9dac505f1c52fb2925a8cd701c23fcf5936161e
ivansukhoparov <rbcdaaa@gmail.com>
2024-05-24 23:37:38 +0200

    refactoring: update FetchAdapter

--- src/adapters/http/utils/fetch.adapter.ts
+++ src/adapters/http/utils/fetch.adapter.ts
@@ -1,8 +1,9 @@
 import fetch from "node-fetch";
 import {ApiResponseType} from "../../../types/fetch-binance/input";
 import {ActionTimer} from "../../../common/utils/timer";
+import {HttpAdapterInterface} from "../interfaces/http.adapter.interface";
 
-export class FetchAdapterREF {
+export class FetchAdapter implements HttpAdapterInterface{
 
 	 async request(url: string, init?: any):Promise<any> {
 		try {
