7ac3c1666520ac208883def6d99f2d5655086b7f
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-02 22:41:51 +0200

    refactoring: rename  TradeCoreNew to TradeCore

--- src/composition.root.ts
+++ src/composition.root.ts
@@ -7,7 +7,7 @@ import {SequencesDataSet} from "./services/classes/sequences.data.set";
 import {SymbolsDataSet} from "./services/classes/symbols.data.set";
 import {Container} from "inversify";
 import {appSettings} from "./index";
-import {TradeCoreNew} from "./core/trade.core.new";
+import {TradeCore} from "./core/tradeCore";
 import {MarketService} from "./application/market-service";
 import {IMarketService} from "./application/market.service.interface";
 
@@ -26,5 +26,5 @@ container.bind<IMarketService>(TYPE.MarketService).to(MarketService).inSingleton
 container.bind<AvailableSymbols>(AvailableSymbols).toSelf().inSingletonScope()
 container.bind<SymbolsDataSet>(SymbolsDataSet).toSelf().inSingletonScope()
 container.bind<SequencesDataSet>(SequencesDataSet).toSelf().inSingletonScope()
-container.bind<TradeCoreNew>(TradeCoreNew).toSelf().inSingletonScope()
+container.bind<TradeCore>(TradeCore).toSelf().inSingletonScope()
 
