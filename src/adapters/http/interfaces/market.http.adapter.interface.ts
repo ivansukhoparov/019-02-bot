44c9f405581c17f7121c41a03bd78844a6cb324a
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-02 22:25:57 +0200

    refactoring: add all methods to MarketHttpAdapterInterface

--- src/adapters/http/interfaces/market.http.adapter.interface.ts
+++ src/adapters/http/interfaces/market.http.adapter.interface.ts
@@ -1,6 +1,36 @@
-import {ApiResponseType} from "../../../types/fetch-binance/input";
+import {ApiResponseType, OrderSide, OrderTypeType} from "../../../types/fetch-binance/input";
 
-export interface MarketHttpAdapterInterface{
-     getAllSymbols(): Promise<ApiResponseType>
-     getTickerPrices(): Promise<ApiResponseType>
+export interface MarketHttpAdapterInterface {
+
+
+    getAccountInfo(): Promise<ApiResponseType>
+
+    getTickerPrices(): Promise<ApiResponseType>
+
+    placeOrder(symbol: string,
+               quantityType: "quantity" | "quoteOrderQty",
+               quantityAmount: number,
+               side: OrderSide,
+               type?: OrderTypeType): Promise<ApiResponseType>
+
+    getAllSymbols(): Promise<ApiResponseType>
+
+    getAllAvailableTickers(): Promise<any>
+
+    getAllWallets(): Promise<any>
+
+    convertCurrency(quoteAsset: string, baseAsset: string, amount: number): Promise<any>
+
+    getCurrencyBalance(currency: string): Promise<any>
+
+    getSymbolInfo(symbol: string): Promise<any>
+
+    getSymbolsInfo(symbols: string[]): Promise<any>
+
+    _createQueryFilter(...filters: any): string
+
+    _createSignature(queryString: string, secret: string, algorithm: string): string
+
+    _responseMapper(response: any): ApiResponseType
+
 }
