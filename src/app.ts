9234a7f62a362c8e21981f1791cd6c2fab51ccf8
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-03 00:05:05 +0200

    Resolve merge conflict by incorporating both suggestions

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,5 @@ import {wsUpdate} from "./base/adapters/markets/binance/binance.websoket";
 
 
 export const app = async () => {
-    // const binanceCore = await init();
-    // console.log("app initiated")
-    // await new Promise(resolve => setTimeout(resolve, 1000));
-    // wsUpdate(binanceCore);
+
 };
