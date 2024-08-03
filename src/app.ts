e3042f33c94295da6b36fef9416a61ca50c64e08
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-04 01:50:49 +0200

    refactoring: create init function

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
+import {container} from "./composition.root";
+import {AvailableSymbols} from "./base/services/data.sets/available.symbols";
+import {SymbolsDataSet} from "./base/services/data.sets/symbols.data.set";
+import {SequencesDataSet} from "./base/services/data.sets/sequences.data.set";
 import {wsUpdate} from "./base/adapters/markets/binance/binance.websoket";
+import {TradeCore} from "./core/trade.core";
+
+const availableSymbols = container.resolve<AvailableSymbols>(AvailableSymbols)
+const symbolsDataSet = container.resolve<SymbolsDataSet>(SymbolsDataSet)
+const sequencesDataSet = container.resolve<SequencesDataSet>(SequencesDataSet)
+const tradeCore = container.resolve<TradeCore>(TradeCore)
+export const appInit = async () => {
+    await availableSymbols.init()
+    console.log(new Date().toISOString(), "Symbols available to trade has been received from server")
+    console.log("Received " + availableSymbols.get().length + " coins available for trade")
+
+    symbolsDataSet.init()
+    console.log(new Date().toISOString(), "Raw data has been formatted")
 
 
-export const app = async () => {
+    sequencesDataSet.init()
+    console.log(new Date().toISOString(), "Sequences has been created")
+    console.log("Created " + sequencesDataSet.get().length + " sequences")
 
+
+    wsUpdate(tradeCore)
 };
