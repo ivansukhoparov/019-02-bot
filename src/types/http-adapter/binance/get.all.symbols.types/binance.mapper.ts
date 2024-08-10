e540bf5bafd19e19c3cbd9e093e5ca1acc36fd4b
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-10 19:43:20 +0200

    fix: Fix the bug where the app crashes if the symbol's status is not 'TRADING'

--- src/types/http-adapter/binance/get.all.symbols.types/binance.mapper.ts
+++ src/types/http-adapter/binance/get.all.symbols.types/binance.mapper.ts
@@ -2,18 +2,32 @@ import {FilterTypes, GetAllSymbolsInputType} from "./binance.input";
 import {GetAllSymbolsOutputType} from "./output";
 
 export const getAllSymbolsBinanceMapper = (input: GetAllSymbolsInputType): GetAllSymbolsOutputType => {
+    const filters = input.filters.reduce((acc: any, filter: any) => {
+        switch (filter.filterType) {
+            case "NOTIONAL":
+                acc.minNotional = filter.minNotional
+                break;
+            case "LOT_SIZE":
+                acc.minQty = filter.minQty
+                break;
+            case "MARKET_LOT_SIZE":
+                acc.minQtyMarket = filter.minQty
+                break;
+            case "LOT_SIZE":
+                acc.stepSize = filter.stepSize
+                acc.stepSizeMarket = filter.minNotional
+                break;
+        }
+        return acc
+    }, {})
+
+
     return {
         symbol: input.symbol,
         status: input.status,
         baseAsset: input.baseAsset,
         quoteAsset: input.quoteAsset,
-        filters: {
-            minNotional: input.filters.find((filter: FilterTypes) => filter.filterType === "NOTIONAL")!["minNotional"],
-            minQty: input.filters.find((filter: FilterTypes) => filter.filterType === "LOT_SIZE")!["minQty"],
-            minQtyMarket: input.filters.find((filter: FilterTypes) => filter.filterType === "MARKET_LOT_SIZE")!["minQty"],
-            stepSize: input.filters.find((filter: FilterTypes) => filter.filterType === "LOT_SIZE")!["stepSize"],
-            stepSizeMarket: input.filters.find((filter: FilterTypes) => filter.filterType === "LOT_SIZE")!["stepSize"],
-        }
+        filters
     };
 };
 
