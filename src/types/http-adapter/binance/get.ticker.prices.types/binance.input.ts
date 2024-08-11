5a5a0dad5f7d8bdbb7d998eabec856f194dca99a
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-12 00:34:03 +0200

    add: The input type for the getTickerPrices method in HttpAdapter for Binance has been added

---
+++ src/types/http-adapter/binance/get.ticker.prices.types/binance.input.ts
@@ -0,0 +1,18 @@
+export type GetGetTickerPricesInputTypeBinance =
+    {
+        "symbol": string
+        "price": string
+    }
+
+
+// [
+//   {
+//     "symbol": "ETHBTC",
+//     "price": "0.04364000"
+//   },
+//   {
+//     "symbol": "LTCBTC",
+//     "price": "0.00101600"
+//   }
+// ...
+// ]
