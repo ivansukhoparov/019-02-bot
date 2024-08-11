b1d186218e1447dc349dc5dd8be6dc01821c985e
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-12 00:49:34 +0200

    refactoring: A statement defining which HttpAdapter class will be bound to MarketHttpAdapter has been added

--- src/composition.root.ts
+++ src/composition.root.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import {Container} from "inversify";
-import {AppSettings} from "./settings/settings";
+import {appSettings} from "./settings/settings";
 import {IHttpAdapter} from "./base/interfaces/http.adapter.interface";
 import {FetchAdapter} from "./base/adapters/common/fetch.adapter";
 import {IMarketHttpAdapter} from "./base/interfaces/market.http.adapter.interface";
@@ -10,24 +10,30 @@ import {MarketService} from "./base/services/market-service";
 import {AvailableSymbols} from "./base/services/data.sets/available.symbols";
 import {SymbolsDataSet} from "./base/services/data.sets/symbols.data.set";
 import {SequencesDataSet} from "./base/services/data.sets/sequences.data.set";
-import {TradeCore} from "./core/trade.core";
+import {HuobiHttpAdapter} from "./base/adapters/markets/binance/huobi.http.adapter";
 
 
-export const TYPE ={
+export const TYPE = {
     HttpAdapter: "HttpAdapter",
     MarketHttpAdapter: "MarketHttpAdapter",
-    MarketService:"MarketService",
+    MarketService: "MarketService",
 }
 
 export const container = new Container()
 
- container.bind<IHttpAdapter>("HttpAdapter").to(FetchAdapter).inSingletonScope()
- container.bind<IMarketHttpAdapter>("MarketHttpAdapter").to(BinanceHttpAdapter).inSingletonScope()
+container.bind<IHttpAdapter>("HttpAdapter").to(FetchAdapter).inSingletonScope()
+
+if (appSettings.marketName === "BINANCE") {
+    container.bind<IMarketHttpAdapter>("MarketHttpAdapter").to(BinanceHttpAdapter).inSingletonScope()
+} else if (appSettings.marketName === "HUOBI") {
+    container.bind<IMarketHttpAdapter>("MarketHttpAdapter").to(HuobiHttpAdapter).inSingletonScope()
+}
+
 container.bind<IMarketService>('MarketService').to(MarketService).inSingletonScope()
 
- container.bind<AvailableSymbols>(AvailableSymbols).toSelf().inSingletonScope()
- container.bind<SymbolsDataSet>(SymbolsDataSet).toSelf().inSingletonScope()
- container.bind<SequencesDataSet>(SequencesDataSet).toSelf().inSingletonScope()
+container.bind<AvailableSymbols>(AvailableSymbols).toSelf().inSingletonScope()
+container.bind<SymbolsDataSet>(SymbolsDataSet).toSelf().inSingletonScope()
+container.bind<SequencesDataSet>(SequencesDataSet).toSelf().inSingletonScope()
 
 // container.bind<TradeCore>(TradeCore).toSelf().inSingletonScope()
 
